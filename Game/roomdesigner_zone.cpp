c4ebe26667b6b6b44f3488ba4342eb4e58dbdb2e
Marq Watkin <devhead@pmprog.co.uk>
2015-07-10 13:18:25 +0100

    s

--- Game/roomdesigner_zone.cpp
+++ Game/roomdesigner_zone.cpp
@@ -4,6 +4,41 @@
 
 void RoomDesigner::ZoneEvent(Event *e)
 {
+  if( e->Type == EVENT_KEY_DOWN )
+  {
+    switch( e->Data.Keyboard.KeyCode )
+    {
+      case ALLEGRO_KEY_INSERT:
+        break;
+      case ALLEGRO_KEY_DELETE:
+        break;
+      case ALLEGRO_KEY_PGUP:
+        break;
+      case ALLEGRO_KEY_PGDN:
+        break;
+      case ALLEGRO_KEY_LEFT:
+        break;
+      case ALLEGRO_KEY_RIGHT:
+        break;
+      case ALLEGRO_KEY_UP:
+        break;
+      case ALLEGRO_KEY_DOWN:
+        break;
+    }
+  }
+
+  if( e->Type == EVENT_KEY_UP )
+  {
+    switch( e->Data.Keyboard.KeyCode )
+    {
+      case ALLEGRO_KEY_LEFT:
+      case ALLEGRO_KEY_RIGHT:
+        break;
+      case ALLEGRO_KEY_UP:
+      case ALLEGRO_KEY_DOWN:
+        break;
+    }
+  }
 }
 
 void RoomDesigner::ZoneUpdate()
